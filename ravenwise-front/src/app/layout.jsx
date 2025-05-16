b4ee24e0b54d8ee8b2220086f5ec2e016aa0d1d4
théau Yapi <theau.yapi@ynov.com>
2025-05-16 09:25:32 +0200

    remove backend

--- ravenwise-front/src/app/layout.jsx
+++ ravenwise-front/src/app/layout.jsx
@@ -1,16 +1,21 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider as CustomClerkProvider } from '../context/clerkContext';
 import './globals.css';
 
 export const metadata = {
   title: 'RavenWise | la plateforme d\'apprentissage adaptatif',
-  description: 'RavenWise est une plateforme d\'apprentissage innovante qui utilise l\'intelligence artificielle pour personnaliser votre parcours éducatif et vous aider à atteindre vos objectifs.',
+  description: 'RavenWise est une plateforme d\'apprentissage innovante...',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey="pk_test_Y3VkZGx5LXNwb25nZS00My5jbGVyay5hY2NvdW50cy5kZXYk">
       <html lang="fr">
-        <body>{children}</body>
+        <body>
+          <CustomClerkProvider>
+            {children}
+          </CustomClerkProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- ravenwise_back/utils/asyncHandler.js
+++
@@ -1,5 +0,0 @@
-// Wrapper pour capturer les erreurs dans les fonctions asynchrones
-const asyncHandler = fn => (req, res, next) =>
-  Promise.resolve(fn(req, res, next)).catch(next);
-
-module.exports = asyncHandler;
